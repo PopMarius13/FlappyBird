701badfd227100122ddf39a16ee8227cb86e4154
PopMarius13 <popmariusionut13@gmail.com>
2020-04-10 20:17:09 +0200

    Flappy

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FlappyBird
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.butonstart = new System.Windows.Forms.Button();
+            this.scorbird = new System.Windows.Forms.Label();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Timers.Timer();
+            this.timer2 = new System.Timers.Timer();
+            this.listaimagini = new System.Windows.Forms.ImageList(this.components);
+            this.timer3 = new System.Timers.Timer();
+            this.textendgame = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // butonstart
+            // 
+            this.butonstart.BackColor = System.Drawing.Color.Yellow;
+            this.butonstart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.butonstart.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butonstart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.butonstart.Location = new System.Drawing.Point(213, 296);
+            this.butonstart.Name = "butonstart";
+            this.butonstart.Size = new System.Drawing.Size(158, 57);
+            this.butonstart.TabIndex = 0;
+            this.butonstart.Text = "Start";
+            this.butonstart.UseVisualStyleBackColor = false;
+            this.butonstart.Click += new System.EventHandler(this.butonstart_Click);
+            // 
+            // scorbird
+            // 
+            this.scorbird.AutoSize = true;
+            this.scorbird.BackColor = System.Drawing.Color.Transparent;
+            this.scorbird.Font = new System.Drawing.Font("Brush Script MT", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorbird.ForeColor = System.Drawing.Color.Blue;
+            this.scorbird.Location = new System.Drawing.Point(269, 9);
+            this.scorbird.Name = "scorbird";
+            this.scorbird.Size = new System.Drawing.Size(47, 59);
+            this.scorbird.TabIndex = 1;
+            this.scorbird.Text = "0";
+            this.scorbird.Click += new System.EventHandler(this.scorbird_Click);
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.BackgroundImage = global::FlappyBird.Properties.Resources.bird_straight;
+            this.bird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bird.Location = new System.Drawing.Point(260, 297);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(65, 55);
+            this.bird.TabIndex = 2;
+            this.bird.TabStop = false;
+            this.bird.Visible = false;
+            this.bird.Click += new System.EventHandler(this.bird_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 38D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 150D;
+            this.timer2.SynchronizingObject = this;
+            this.timer2.Elapsed += new System.Timers.ElapsedEventHandler(this.timer2_Elapsed);
+            // 
+            // listaimagini
+            // 
+            this.listaimagini.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("listaimagini.ImageStream")));
+            this.listaimagini.TransparentColor = System.Drawing.Color.Transparent;
+            this.listaimagini.Images.SetKeyName(0, "birdcade.png");
+            this.listaimagini.Images.SetKeyName(1, "birdzboara.png");
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1D;
+            this.timer3.SynchronizingObject = this;
+            this.timer3.Elapsed += new System.Timers.ElapsedEventHandler(this.timer3_Elapsed);
+            // 
+            // textendgame
+            // 
+            this.textendgame.AutoSize = true;
+            this.textendgame.BackColor = System.Drawing.Color.Transparent;
+            this.textendgame.Font = new System.Drawing.Font("Modern No. 20", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textendgame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textendgame.Location = new System.Drawing.Point(25, 170);
+            this.textendgame.Name = "textendgame";
+            this.textendgame.Size = new System.Drawing.Size(86, 29);
+            this.textendgame.TabIndex = 3;
+            this.textendgame.Text = "label1";
+            this.textendgame.Visible = false;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::FlappyBird.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(582, 650);
+            this.Controls.Add(this.textendgame);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.scorbird);
+            this.Controls.Add(this.butonstart);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.butonstart_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butonstart;
+        private System.Windows.Forms.Label scorbird;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Timers.Timer timer1;
+        private System.Timers.Timer timer2;
+        private System.Windows.Forms.ImageList listaimagini;
+        private System.Timers.Timer timer3;
+        private System.Windows.Forms.Label textendgame;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using FlappyBird.Properties;
+using System.Threading;
+using System.Media;
+
+namespace FlappyBird
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+        int spatiuconducte = 210;
+        int latimeconducte = 65;
+        int pozinitialax=260;
+        int pozinitialay=279;
+        int inaltimebird=25;
+        int scor = 0;
+        bool okd1 = true;
+        bool okd2 = true;
+        int scormax;
+        int v = 1;
+        double p = 0;
+        Rectangle[] dreptunghisus;
+        Rectangle[] dreptunghijos;
+        Rectangle dreptunghibird;
+        Random random = new Random();
+        int r;
+
+        private void butonstart_Load(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader("scormax1.txt"))
+            {
+                StreamReader sr = File.OpenText("scormax1.txt");
+                scormax = sr.Read();
+                reader.Close();
+            }
+                bird.Location = new Point(pozinitialax, pozinitialay);
+                dreptunghisus = new Rectangle[7];
+                dreptunghijos = new Rectangle[7];
+                scormax = scormax - '0';
+                textendgame.Text = Convert.ToString(scormax);
+        }
+        private void butonstart_Click(object sender, EventArgs e)
+        {
+            startgame();
+        }
+
+        private void scorbird_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bird_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void timer2_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            bird.Location = new Point(bird.Location.X, bird.Location.Y + inaltimebird);
+           
+        }
+
+        private void timer3_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (scor1())
+            {
+                SoundPlayer sp1 = new SoundPlayer(FlappyBird.Properties.Resources.point);
+                sp1.Play();
+                scor++;
+                scorbird.Text = Convert.ToString(scor);
+                p = p + 0.05;
+                if(p==1)
+                {
+                    v = v + 1;
+                    p = 0;
+                }
+            }
+            if (mort())
+                endgame();
+        }
+        private void form1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color.Green);
+            if (butonstart.Visible)
+            {
+                r = random.Next(40, this.Height - 290);
+                dreptunghisus[1] = new Rectangle(this.Width, 0, latimeconducte, r);
+                dreptunghijos[1] = new Rectangle(this.Width, r + spatiuconducte, latimeconducte, this.Height - r - spatiuconducte);
+                dreptunghisus[3] = new Rectangle(this.Width-10, r, latimeconducte + 20, 15);
+                dreptunghijos[3] = new Rectangle(this.Width-10, r + spatiuconducte, latimeconducte + 20, 15);
+
+
+                r = random.Next(40, this.Height - 290);
+                dreptunghisus[2] = new Rectangle(this.Width +300+latimeconducte/2, 0, latimeconducte, r);
+                dreptunghijos[2] = new Rectangle(this.Width +300 + latimeconducte / 2, r + spatiuconducte, latimeconducte, this.Height - r - spatiuconducte);
+                dreptunghisus[4] = new Rectangle(this.Width +290 + latimeconducte / 2, r, latimeconducte + 20, 15);
+                dreptunghijos[4] = new Rectangle(this.Width +290 + latimeconducte / 2, r + spatiuconducte, latimeconducte + 20, 15);
+                
+                
+            }
+                for(int i=1;i<=2;i++)
+                    {
+                        dreptunghijos[i].X = dreptunghijos[i].X - v;
+                        dreptunghisus[i].X = dreptunghisus[i].X - v;
+
+                        dreptunghijos[i+2].X = dreptunghijos[i+2].X - v;
+                        dreptunghisus[i+2].X = dreptunghisus[i+2].X - v;
+
+                        dreptunghijos[i + 4].X = dreptunghijos[i + 4].X - v;
+                        dreptunghisus[i + 4].X = dreptunghisus[i + 4].X - v;
+                    }
+                 if(dreptunghijos[1].X+10<-latimeconducte)
+                    {
+                r = random.Next(40, this.Height - 290);
+                dreptunghisus[1] = new Rectangle(this.Width, 0, latimeconducte, r);
+                dreptunghijos[1] = new Rectangle(this.Width, r + spatiuconducte, latimeconducte, this.Height - r - spatiuconducte);
+                dreptunghisus[3] = new Rectangle(this.Width -10, r, latimeconducte + 20, 15);
+                dreptunghijos[3] = new Rectangle(this.Width -10, r + spatiuconducte, latimeconducte + 20, 15);
+                okd1 = true;
+            }
+                if(dreptunghijos[2].X+10<-latimeconducte)                
+                    {
+                okd2 = true;
+
+                r = random.Next(40, this.Height - 290);
+                dreptunghisus[2] = new Rectangle(this.Width , 0, latimeconducte, r);
+                dreptunghijos[2] = new Rectangle(this.Width , r + spatiuconducte, latimeconducte, this.Height - r - spatiuconducte);
+                dreptunghisus[4] = new Rectangle(this.Width-10 , r, latimeconducte + 20, 15);
+                dreptunghijos[4] = new Rectangle(this.Width-10 , r + spatiuconducte, latimeconducte + 20, 15);
+            }
+             
+            e.Graphics.FillRectangles(brush, dreptunghisus);
+            e.Graphics.FillRectangles(brush, dreptunghijos);
+            
+        }
+        public void startgame ()
+        {
+            bird.Location = new Point(pozinitialax, pozinitialay);
+            butonstart.Enabled = false;
+            butonstart.Visible = false;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            bird.Visible = true;
+            scor = 0;
+            scorbird.Text = Convert.ToString(scor);
+            scorbird.Text = "0";
+            textendgame.Visible = false;
+        }
+        public void endgame()
+        {
+            if (scor > scormax)
+            {
+                SoundPlayer sp2 = new SoundPlayer(FlappyBird.Properties.Resources.lightapplause);
+                sp2.Play();
+                using (StreamWriter writer = new StreamWriter("scormax1.txt"))
+                {
+                    scormax = scor;
+                    writer.Write(scor);
+                    textendgame.Text = "Felicitari!!!\nAi facut un scor maxim " + scor + " puncte";
+                    writer.Close();
+                }
+            }
+            else
+            {
+                textendgame.Text = "Mai exerseaza.Ai facut " + scor + " puncte\nScor maxim " + scormax + " puncte";
+                SoundPlayer sp = new SoundPlayer(FlappyBird.Properties.Resources.footballcrowd2);
+                sp.Play();
+            }
+                butonstart.Visible = true;
+                butonstart.Enabled = true;
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                bird.Visible = false;
+                scor = 0;
+                okd1 = true;
+                okd2 = true;
+                textendgame.Visible = true;
+                
+        }
+        private void form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                inaltimebird = -30;
+                bird.BackgroundImage = listaimagini.Images[1];
+            }
+        }
+
+        private void form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                inaltimebird = 25;
+                bird.BackgroundImage = listaimagini.Images[0];
+            }
+        }
+        private bool intersectie ()
+        {
+            bool ok = false;
+            for (int i = 1; i <= 4; i++)
+            {
+                if (Rectangle.Intersect(dreptunghibird, dreptunghijos[i]) != Rectangle.Empty)
+                    ok = true;
+                else
+                    if (Rectangle.Intersect(dreptunghibird, dreptunghisus[i]) != Rectangle.Empty)
+                    ok = true;
+            }
+            if (dreptunghibird.Y + 55 > this.Height)
+                ok = true;
+            if (dreptunghibird.Y +55 < 0)
+                ok = true; 
+           return ok;
+        }
+        private bool mort ()
+        {
+            bool ok = false;
+            dreptunghibird = bird.Bounds;
+            if (intersectie())
+                ok = true;
+            return ok;
+        }
+        private bool scor1()
+        {
+            if (dreptunghijos[1].X < pozinitialax - dreptunghibird.Height && okd1)
+            {
+                okd1 = false;
+                 return true;
+            }
+            else
+           if (dreptunghijos[1].X < pozinitialax - dreptunghibird.Height + 1 && okd1)
+            {
+                okd1 = false;
+                return true;
+            }
+            if (dreptunghijos[2].X < pozinitialax - dreptunghibird.Height && okd2)
+            {
+                okd2 = false;
+                return true;
+            }
+            else
+           if (dreptunghijos[2].X < pozinitialax - dreptunghibird.Height + 1 && okd2)
+            {
+                okd2 = false;
+                return true;
+            }
+            return false;
+        }
+    }
+}
